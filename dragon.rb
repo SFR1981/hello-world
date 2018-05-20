dbf1e8454c7fb0a5ea813f385b8444d978895803
Stephen Rooney <s.f.rooney@gmail.com>
2018-05-20 17:35:13 +0100

    Glorius update

--- dragon.rb
+++ dragon.rb
@@ -4,10 +4,10 @@ class Dragon
 			@name = name
 			@asleep = false
 			@stuff_in_belly = 10 #full
-			@stuff_in_intestine = 0 # He doesn't need the toilet. 
+			@stuff_in_intestine = 0 # He doesn't need the toilet.
 
 
-			puts "#{@name} is born at #{Time.new}."
+			puts "#{@name} is born at #{Time.new}. It is a glorius day!"
 
 
 
